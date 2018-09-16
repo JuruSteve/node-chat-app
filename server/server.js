95513753f13b8a76fd197e54e8cb2fd85adf25a7
Steve Mugenzi <steve@Steves-MacBook-Air.local>
2018-09-16 18:01:16 -0400

    set up connection events

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const path = require('path');
 const express = require('express');
-
-const publicPath = path.join(__dirname, '../public');
-
 const app = express();
-const port = process.env.PORT || 3000
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
+const port = process.env.PORT || 3000
+const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+    socket.on('disconnect', ()=>{
+        console.log('User was disconnected from server')
+    })
+})
+
+server.listen(port, ()=>{
     console.log(`server started at *: ${port}`);
 })
